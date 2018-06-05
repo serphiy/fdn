b55419a39d9e16d0373ca21070571f52a91f7dcb
Serhii Ralchenko <ralchenkos@gmail.com>
2018-06-06 02:40:48 +0300

    rewrote carousels init

--- source/js/main.js
+++ source/js/main.js
@@ -1,79 +1,47 @@
 $(document).ready(function(){
-  // page-intro carousel
-  $('.page-intro__carousel').on('init', function(event, slick){
-    var carouselControls = $('.page-intro__controls');
-    carouselControls.find('.carousel-numbers__current-number')
-                    .text(parseInt(slick.currentSlide + 1).toString().padStart(2, "0"));
-    carouselControls.find('.carousel-numbers__total-number')
-                    .text('/' + slick.slideCount.toString().padStart(2, "0"));
-  });
+  // carousels
+  var carouselSelectors = {
+    '.page-intro__carousel':      '.page-intro__controls',
+    '.services-carousel':         '.services-controls',
+    '.page-portfolio__carousel':  '.page-portfolio__controls'
+  };
 
-  $('.page-intro__carousel').on('afterChange', function(event, slick, currentSlide){
-    var carouselControls = $('.page-intro__controls');
-    carouselControls.find('.carousel-numbers__current-number')
-                    .text(parseInt(slick.currentSlide + 1).toString().padStart(2, "0"));
-    carouselControls.find('.carousel-numbers__total-number')
-                    .text('/' + slick.slideCount.toString().padStart(2, "0"));
-  });
-  $('.page-intro__carousel').slick({
-      arrows: true,
-      dots: false,
-      infinite: true,
-      speed: 300,
-      slidesToShow: 1,
-      adaptiveHeight: true,
-      appendArrows: $('.page-intro__controls').find('.carousel-controls')
-  });
+  function findControls(event, carouselSelectors) {
+    var carouselControls = null;
+      for (var selector in carouselSelectors) {
+        if ($(event.target).hasClass(selector.slice(1))) {
+          carouselControls = $(carouselSelectors[selector]);
+          break;
+        }
+      }
+    return carouselControls;
+  }
 
-  // services carousel
-  $('.services-carousel').on('init', function(event, slick){
-    var carouselControls = $('.services-controls');
-    carouselControls.find('.carousel-numbers__current-number')
-                    .text(parseInt(slick.currentSlide + 1).toString().padStart(2, "0"));
-    carouselControls.find('.carousel-numbers__total-number')
-                    .text('/' + slick.slideCount.toString().padStart(2, "0"));
-  });
+  for (var carousel in carouselSelectors) {
+    $(carousel).on('init', function(event, slick){
+      var carouselControls = findControls(event, carouselSelectors);
+      carouselControls.find('.carousel-numbers__current-number')
+                      .text(('00' + parseInt(slick.currentSlide + 1)).slice(-2));
+      carouselControls.find('.carousel-numbers__total-number')
+                      .text('/' + ('00' + slick.slideCount).slice(-2));
+    });
 
-  $('.services-carousel').on('afterChange', function(event, slick, currentSlide){
-    var carouselControls = $('.services-controls');
-    carouselControls.find('.carousel-numbers__current-number')
-                    .text(parseInt(slick.currentSlide + 1).toString().padStart(2, "0"));
-    carouselControls.find('.carousel-numbers__total-number')
-                    .text('/' + slick.slideCount.toString().padStart(2, "0"));
-  });
-  $('.services-carousel').slick({
-      arrows: true,
-      dots: false,
-      infinite: true,
-      speed: 300,
-      slidesToShow: 1,
-      adaptiveHeight: true,
-      appendArrows: $('.services-controls').find('.carousel-controls')
-  });
+    $(carousel).on('afterChange', function(event, slick, currentSlide){
+      var carouselControls = findControls(event, carouselSelectors);
+      carouselControls.find('.carousel-numbers__current-number')
+                      .text(('00' + parseInt(slick.currentSlide + 1)).slice(-2));
+      carouselControls.find('.carousel-numbers__total-number')
+                      .text('/' + ('00' + slick.slideCount).slice(-2));
+    });
 
-  // portfolio carousel
-  $('.page-portfolio__carousel').on('init', function(event, slick){
-    var carouselControls = $('.page-portfolio__controls');
-    carouselControls.find('.carousel-numbers__current-number')
-                    .text(parseInt(slick.currentSlide + 1).toString().padStart(2, "0"));
-    carouselControls.find('.carousel-numbers__total-number')
-                    .text('/' + slick.slideCount.toString().padStart(2, "0"));
-  });
-
-  $('.page-portfolio__carousel').on('afterChange', function(event, slick, currentSlide){
-    var carouselControls = $('.page-portfolio__controls');
-    carouselControls.find('.carousel-numbers__current-number')
-                    .text(parseInt(slick.currentSlide + 1).toString().padStart(2, "0"));
-    carouselControls.find('.carousel-numbers__total-number')
-                    .text('/' + slick.slideCount.toString().padStart(2, "0"));
-  });
-  $('.page-portfolio__carousel').slick({
-      arrows: true,
-      dots: false,
-      infinite: true,
-      speed: 300,
-      slidesToShow: 1,
-      adaptiveHeight: true,
-      appendArrows: $('.page-portfolio__controls').find('.carousel-controls')
-  });
+    $(carousel).slick({
+        arrows: true,
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        adaptiveHeight: true,
+        appendArrows: $(carouselSelectors[carousel]).find('.carousel-controls')
+    });
+  }
 });
